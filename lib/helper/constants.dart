ee5825144b8eb4a76522530f3a9e927ea194b1b9
Ahmed Yousef <ahmed.moyousef98@gmail.com>
2025-05-03 17:30:29 +0300

    privacy policy updated

--- lib/helper/constants.dart
+++ lib/helper/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
   static const String privacyPolicyUrl =
-      'https://drive.google.com/file/d/1ecEDIllm4hjHIBl8nNaQFgDE6qhtuSBS/view?usp=sharing';
+      'https://github.com/ahmedmoyousef/Connectly/blob/main/README_PRIVACY_POLICY.md';
 }
